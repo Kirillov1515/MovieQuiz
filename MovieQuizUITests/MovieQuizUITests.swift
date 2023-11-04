228b314a5f0ebbfd74df7e8efae01105dc61cdf4
Kirillov Alexander <kirillov_sasha98@mail.ru>
2023-11-04 17:11:39 +0300

    Начало работы над спринтом 7

---
+++ MovieQuizUITests/MovieQuizUITests.swift
@@ -0,0 +1,33 @@
+//
+//  MovieQuizUITests.swift
+//  MovieQuizUITests
+//
+//  Created by Macbook on 04.11.2023.
+//
+
+import XCTest
+
+final class MovieQuizUITests: XCTestCase {
+    
+    var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        app = XCUIApplication()
+        app.launch()
+
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+        try super.tearDownWithError()
+        
+        app.terminate()
+        app = nil
+    }
+    
+    func testScreenCast() throws {
+        
+    }
+}
